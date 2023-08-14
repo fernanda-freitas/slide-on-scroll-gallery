b49ba5f53f8681ceddf0defa50fbc9020b409260
Fernanda Freitas <idea.fernanda@gmail.com>
2023-08-14 10:40:32 +0100

    animatins to column

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { motion, useScroll } from "framer-motion";
-// import { useEffect } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const images = [
@@ -13,29 +13,47 @@ export default function Home() {
     "zanad02.png",
   ];
 
-  const { scrollYProgress } = useScroll();
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1 0"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [0, 2000]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, 1000]);
+
+  const [dimension, setDimension] = useState({ height: 0 });
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setDimension();
+    });
+  }, []);
+
+  console.log(dimension);
 
   return (
     <>
       <section className={styles.section}></section>
-      <section className={styles.section}>
+      <section ref={ref} className={styles.section}>
         <div className={styles.galleryholder}>
-          {images.map((src, i) => {
-            return (
-              <motion.div
-                style={{ top: scrollYProgress }}
-                key={i}
-                className={styles.imageholder}
-              >
-                <Image
-                  src={`/images/${src}`}
-                  className={styles.galleryimage}
-                  fill
-                  alt="image"
-                ></Image>
-              </motion.div>
-            );
-          })}
+          <motion.div style={{ y }} className={styles.imageholder}>
+            <Image
+              src={`/images/${images[0]}`}
+              className={styles.galleryimage}
+              fill
+              alt="image"
+            ></Image>
+          </motion.div>
+          <motion.div style={{ y2 }} className={styles.imageholder}>
+            <Image
+              src={`/images/${images[0]}`}
+              className={styles.galleryimage}
+              fill
+              alt="image"
+            ></Image>
+          </motion.div>
         </div>
       </section>
       <section className={styles.section}></section>
