2c0657676612a6e9708e8481b8c6a945f11e9907
Fernanda Freitas <idea.fernanda@gmail.com>
2023-08-14 11:32:20 +0100

    Refine animations on columns

--- src/app/page.js
+++ src/app/page.js
@@ -20,15 +20,21 @@ export default function Home() {
     offset: ["0 1", "1 0"],
   });
 
-  const y = useTransform(scrollYProgress, [0, 1], [0, 2000 * 0.2]);
-  const y2 = useTransform(scrollYProgress, [0, 1], [0, 1000]);
+  const y = useTransform(scrollYProgress, [0, 1], [0, -1000 * 0.6]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, 2000]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, 1000 * 0.6]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, 1800 * 0.6]);
+  const y5 = useTransform(scrollYProgress, [0, 1], [0, 2000]);
 
   return (
     <>
       <section className={styles.section}></section>
       <section ref={ref} className={styles.section}>
         <div className={styles.galleryholder}>
-          <motion.div style={{ top: y }} className={styles.imageholder}>
+          <motion.div
+            style={{ top: y }}
+            className={`${styles.imageholder} ${styles.image1}`}
+          >
             <Image
               src={`/images/${images[0]}`}
               className={styles.galleryimage}
@@ -36,9 +42,45 @@ export default function Home() {
               alt="image"
             ></Image>
           </motion.div>
-          <motion.div style={{ top: y2 }} className={styles.imageholder}>
+          <motion.div
+            style={{ top: y2 }}
+            className={`${styles.imageholder} ${styles.image2}`}
+          >
             <Image
               src={`/images/${images[1]}`}
+              className={styles.galleryimage}
+              fill
+              alt="image"
+            ></Image>
+          </motion.div>
+          <motion.div
+            style={{ top: y3 }}
+            className={`${styles.imageholder} ${styles.image3}`}
+          >
+            <Image
+              src={`/images/${images[2]}`}
+              className={styles.galleryimage}
+              fill
+              alt="image"
+            ></Image>
+          </motion.div>
+          <motion.div
+            style={{ top: y4 }}
+            className={`${styles.imageholder} ${styles.image4}`}
+          >
+            <Image
+              src={`/images/${images[3]}`}
+              className={styles.galleryimage}
+              fill
+              alt="image"
+            ></Image>
+          </motion.div>
+          <motion.div
+            style={{ top: y5 }}
+            className={`${styles.imageholder} ${styles.image5}`}
+          >
+            <Image
+              src={`/images/${images[4]}`}
               className={styles.galleryimage}
               fill
               alt="image"
